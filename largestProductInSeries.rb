676cfd4be74b3268c54523e76b1f179481b92ef7
Lamberto C. Diwa <lambertodiwajr@gmail.com>
2013-06-12 22:06:21 -0500

    add smallest multiple

--- largestProductInSeries.rb
+++ largestProductInSeries.rb
@@ -1,24 +1,70 @@
 require 'minitest/autorun'
+require 'stringio'
 
 class TestLargestProductInSeries < MiniTest::Unit::TestCase
 
-	def setup
-		@largest_product_in_series = LargestProductInSeries.new
-		@number_text = File.read('input1000digits.txt')
-	end 
+  def setup
+    @largest_product_in_series = LargestProductInSeries.new
+    raw_text = File.read('input1000digits.txt')
+    @number_text = raw_text.gsub(/\n/,"")
+  end 
 
-	def test_read_file
-		puts @number_text
-	end
+  def test_read_file_using_stringio
+    str = StringIO.new(@number_text)
+    line = str.gets
+    puts line 
+  end
 
-	def test_read_5_characters_from_text
-		first_five_in_char = @number_text[0..4]
-		puts "Here are first five characters from file : #{first_five_in_char}"
-		first_five_in_int = first_five_in_char.to_i
-		puts "Class of first_five_in_int is :", first_five_in_int.class
-	end
+  def test_read_file_via_file_read
+    puts
+    puts @number_text
+    puts "lenght of text is: #{@number_text.length}"
+  end
+
+  def test_read_5_characters_using_array_slice
+    first_five_in_char = @number_text[0..4]
+    first_five_in_int = first_five_in_char.to_i
+    assert_equal 73167, first_five_in_int
+    assert_equal "73167", first_five_in_char
+  end
+
+  # def test_reading_5_characters_using_stringio
+  #   five_chars = @str.read(5)
+  #   assert_equal "73167",five_chars
+  # end
+
+  def test_reading_chars_in_iteration
+
+    assert_equal 0, @largest_product_in_series
+  end
+
+  # def test_reading_1_char_at_a_time_until_eof
+  #   position = 0
+  #   while @str.eof? == false
+  #     @str.pos = position
+  #     five_chars = @str.read(5)
+  #     puts five_chars
+  #     position += 1
+  #   end
+  # end
 end
 
 class LargestProductInSeries
+  attr_accessible :product_array
+
+  def initiallze
+    @product_array = []
+  end
+
+  def extract_numbers_from_file_into_array
+
+    index = 0
+    while index < @number_text.length - 4
+      five_digit_num = @number_text[index..index+4]
+      @product_array << five_digit_num
+      # puts "five digit number is : #{five_digit_num}"
+      index += 1
+    end
+  end
 
 end
--- smallestMultiple.rb
+++ smallestMultiple.rb
@@ -16,36 +16,118 @@ class TestSmallestMultiple < MiniTest::Unit::TestCase
     
   end
 
-  def test_multiple_of_two_numbers
-    assert_equal 6, @smallest_multiple.multiple_of_two_numbers(2,3)  
+  def test_number_is_multiple_of_another_number
+    test_number = 3
+    test_multiple = 12
+    assert @smallest_multiple.numbers_are_multiples?(test_multiple, test_number), 
+      "Numbers are not multiples #{test_multiple} : #{test_number}"
   end
 
-  def test_get_common_multiple
-    # common_multiple = 0
-    # (2..9).each do |num|
-    #   multiple = @smallest_multiple.multiple_of_two_numbers(num, num+1)
-    #   while true
-    #     if @smallest_multiple.number_is_a_multiple_of_these_two?(multiple, 2, num+1)
-    #      common_multiple = multiple
-    #      break  
-
-    #   end
-    #    puts "Num1: #{num}, Num2: #{num+1} = Common: #{common_multiple}"
-    # end
+  def test_number_is_not_multiple_of_another_number
+    test_number = 5
+    test_multiple = 12
+    assert @smallest_multiple.numbers_are_multiples?(test_multiple, test_number), 
+      "Numbers are not multiples #{test_multiple} : #{test_number}"
   end
 
-  def test_fill_array_of_multiples_up_to_10
-    array = Array.new(10) { Array.new(10) }
-    (1..10).each do |number|
-      (1..10).each do |multiples|
-        array[number - 1][multiples - 1] = number * multiples
-      end
-    end
+  def test_get_next_multiple
+    last_multiple = 12
+    latest_number = 5
+    assert_equal 60, @smallest_multiple.get_next_multiple(last_multiple, latest_number)
+  end
+
+  def test_successfully_getting_a_lower_multiple
+    latest_multiple = 120
+    number_series = [2, 3, 4, 5, 6]
+    assert_equal 60, @smallest_multiple.return_a_lower_multiple_if_any(latest_multiple, number_series)
+  end
+
+  def test_get_multiple_of_numbers_1_to_10
+    number_series = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert_equal 2520, @smallest_multiple.get_multiple_of_number_series(number_series)
+  end
+
+  def test_get_multiple_of_numbers_1_to_20
+    number_series = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    assert_equal 0, @smallest_multiple.get_multiple_of_number_series(number_series)
   end
 
 end
 
 class SmallestMultiple
+
+  def get_multiple_of_number_series(number_series)
+    multiple = 1
+    number_series.each do |number|
+      sub_series = Array.new(number) { |i| i+1 }
+      puts "sub_series: #{sub_series}"
+      puts "current multiple: #{multiple}  number_to_test: #{number}"
+      if number > 1
+        if number.even? 
+          divide_by = 2 
+        else 
+          divide_by = 3
+        end  
+        multiple = derive_multiple(multiple, number, sub_series, divide_by)
+      end  
+    end
+    multiple
+  end
+
+  def derive_multiple(multiple, number, sub_series, divide_by)
+    multiple_found = false
+    while multiple_found == false
+      if number_is_a_multiple_of_number_series?(multiple, sub_series)
+
+        multiple = return_a_lower_multiple_if_any(multiple, sub_series, divide_by)
+        multiple_found = true
+      else
+        multiple = multiple * number
+      end    
+    end
+    multiple
+  end
+
+  def number_is_a_multiple_of_number_series?(test_number, number_series)
+    remainder = 0
+    (number_series).each do |divisor|
+      remainder += test_number % divisor
+    end
+    if remainder == 0 
+      return true 
+    else 
+      return false
+    end
+  end
+
+  def get_next_multiple(num1, num2)
+    num1 * num2
+  end
+
+  def numbers_are_multiples?(test_multiple, test_number)
+    test_number > 0 || raise("divide by zero error")
+    if test_multiple % test_number == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def return_a_lower_multiple_if_any(multiple_to_test, number_series, divide_by = 2)
+    new_multiple = multiple_to_test
+    while true
+      lower_multiple = multiple_to_test / divide_by
+      puts "multiple_to_test: #{multiple_to_test} lower_multiple: #{lower_multiple}"
+      if number_is_a_multiple_of_number_series?(lower_multiple, number_series)
+        new_multiple = multiple_to_test = lower_multiple
+      else
+        break
+      end
+    end
+    puts "multiple chosen: #{new_multiple}"
+    new_multiple
+  end
+
 
   def is_multiple_of_numbers_1_to_10?(number)
     remainder = 0
@@ -53,21 +135,11 @@ class SmallestMultiple
       remainder += number % divisor
     end
     puts "remainder is #{remainder}"
-    return true if remainder == 0 else return false
-  end
-
-  def multiple_of_two_numbers(num1, num2)
-    num1 * num2
-  end
-
-  def number_is_a_multiple_of_these_two?(test_number, num1, num2)
-    remainder = 0
-    (num1..num2).each do |divisor|
-      remainder += test_number % divisor
+    if remainder == 0 
+      return true 
+    else 
+      return false
     end
-    return true if remainder == 0 else return false
   end
-
-
 
 end
